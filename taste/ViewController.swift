663ba76e1b5a4606792cce21debe9fb50d38ecb5
zyssky <zyssky@zysskydeMac.local>
2016-10-29 13:56:50 +0800

    Initial Commit

---
+++ taste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taste
+//
+//  Created by zyssky on 2016/10/29.
+//  Copyright © 2016年 zyssky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
